d68f167c2061e4f8ac689cfcf62f84a122814c06
Tharindu Wickramanayake <36078972+tdwic@users.noreply.github.com>
2020-10-11 03:53:26 +0530

    activity base created

---
+++ app/src/main/java/com/example/ordermanagementapp/AddProductDetails.java
@@ -0,0 +1,14 @@
+package com.example.ordermanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProductDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product_details);
+    }
+}
