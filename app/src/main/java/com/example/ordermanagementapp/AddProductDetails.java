d945cd97a9f322db4dc5e90fc62b11aeb369ff84
Tharindu Wickramanayake <36078972+tdwic@users.noreply.github.com>
2020-10-11 23:29:01 +0530

    order adding page txt and butoons conigured

--- app/src/main/java/com/example/ordermanagementapp/AddProductDetails.java
+++ app/src/main/java/com/example/ordermanagementapp/AddProductDetails.java
@@ -2,11 +2,20 @@ package com.example.ordermanagementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class AddProductDetails extends AppCompatActivity {
+
+    private Button btn_add_product;
+    private TextView txt_product_code, txt_name, txt_description, txt_unit_price, txt_qty;
+    private Spinner txt_supplier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +27,23 @@ public class AddProductDetails extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
 
         setContentView(R.layout.activity_add_product_details);
+
+        btn_add_product = (Button) findViewById(R.id.add_product);
+        txt_supplier = (Spinner) findViewById(R.id.supplier);
+
+        txt_product_code = (TextView) findViewById(R.id.product_code);
+        txt_name = (TextView) findViewById(R.id.name);
+        txt_description = (TextView) findViewById(R.id.description);
+        txt_unit_price = (TextView) findViewById(R.id.unit_price);
+        txt_qty = (TextView) findViewById(R.id.qty);
+
+        btn_add_product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddProductDetails.this, ProductList.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/ordermanagementapp/NewOrder.java
+++ app/src/main/java/com/example/ordermanagementapp/NewOrder.java
@@ -2,11 +2,18 @@ package com.example.ordermanagementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class NewOrder extends AppCompatActivity {
+
+    private Button btn_upload, btn_add_product_details;
+    private TextView txt_site_manager, txt_location, txt_delivery_notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,30 @@ public class NewOrder extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
 
         setContentView(R.layout.activity_new_order);
+
+        btn_add_product_details = (Button) findViewById(R.id.btn_add_product_details);
+        btn_upload = (Button) findViewById(R.id.btn_upload);
+
+        txt_site_manager = (TextView)findViewById(R.id.txt_sitemanger);
+        txt_location = (TextView)findViewById(R.id.txt_location);
+        txt_delivery_notes = (TextView)findViewById(R.id.txt_delivery_note);
+
+
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn_add_product_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewOrder.this, AddProductDetails.class );
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
