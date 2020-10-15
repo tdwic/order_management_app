80a1a9aab52526902ca6d9c639f858f7bdd44e24
Tharindu Wickramanayake <36078972+tdwic@users.noreply.github.com>
2020-10-15 21:20:45 +0530

    rowselecting enabled

--- app/src/main/java/com/example/ordermanagementapp/ManageRequesitions.java
+++ app/src/main/java/com/example/ordermanagementapp/ManageRequesitions.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ManageRequesitions extends AppCompatActivity {
 
@@ -38,7 +40,8 @@ public class ManageRequesitions extends AppCompatActivity {
         int[] textViewColumnWidth = {180,200,220,200,200};//Each Column Width
         int[] textViewRowHeight = {110, 100};//Header row width , Data Row Width
         int[] headerColorRGB = {54,46,230} ;//RGB ints for table column
-        int[] rowColorRGB = {198,233,255} ;//RGB ints for table row
+        final int[] rowColorRGB = {198,233,255} ;//RGB ints for table row
+        final int[] rowSelectColorRGB = {105,186,255} ;//RGB ints for table row
         int[] paddingLTRB = {10,10,10,10} ;//Padding Left, Top, Right, Bottom
         String[] textViewHeader = {"OrderID","Site","Sitemanger","Total Price","Supplier"}; //Table Column Headers
         String[][] tableData = {
@@ -68,6 +71,31 @@ public class ManageRequesitions extends AppCompatActivity {
         for (int row = 0; row < tableData.length; row++){
 
             tableRowData = new TableRow(this);
+            tableRowData.setClickable(true);
+            tableRowData.setBackgroundColor(Color.rgb(rowColorRGB[0], rowColorRGB[1], rowColorRGB[2]));
+            final Boolean[] isRowSelect = {false};
+
+            tableRowData.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!isRowSelect[0]){
+                        v.setBackgroundColor(Color.rgb(rowSelectColorRGB[0], rowSelectColorRGB[1], rowSelectColorRGB[2]));
+
+                        TableRow selectedRow = (TableRow) v;
+                        TextView textView = (TextView) selectedRow.getChildAt(0);
+                        String result = textView.getText().toString();
+
+                        Toast.makeText(ManageRequesitions.this, result, Toast.LENGTH_SHORT).show();
+
+                        isRowSelect[0] = true;
+
+                    }else {
+                        v.setBackgroundColor(Color.rgb(rowColorRGB[0], rowColorRGB[1], rowColorRGB[2]));
+                        isRowSelect[0] = false;
+                    }
+                }
+            });
+
 
             for (int col = 0; col < tableData[0].length; col++){
                 TextView data = new TextView(this);
@@ -76,7 +104,6 @@ public class ManageRequesitions extends AppCompatActivity {
                 data.setPadding(paddingLTRB[0], paddingLTRB[1], paddingLTRB[2], paddingLTRB[3]);
                 data.setWidth(textViewColumnWidth[col]);
                 data.setHeight(textViewRowHeight[1]);
-                data.setBackgroundColor(Color.rgb(rowColorRGB[0], rowColorRGB[1], rowColorRGB[2]));
                 data.setTextColor(Color.BLACK);
 
                 tableRowData.addView(data);
