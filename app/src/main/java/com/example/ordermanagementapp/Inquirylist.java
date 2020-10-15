eb4da8946966c4b988fded4e443599214413af6f
Dewsara <dewsara6@gmail.com>
2020-10-16 04:05:44 +0530

    Inquiry List UI Created

---
+++ app/src/main/java/com/example/ordermanagementapp/Inquirylist.java
@@ -0,0 +1,119 @@
+package com.example.ordermanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Inquirylist extends AppCompatActivity {
+
+    private TableLayout tableLayout;
+    private TableRow tableRowHeader,tableRowData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
+        setContentView(R.layout.activity_inquirylist);
+
+        table_populate();
+    }
+
+    public void table_populate(){
+
+        tableLayout = (TableLayout) findViewById(R.id.table_main);
+
+        int[] textViewColumnWidth = {180,200,220,200,200};//Each Column Width
+        int[] textViewRowHeight = {110, 100};//Header row width , Data Row Width
+        int[] headerColorRGB = {54,46,230} ;//RGB ints for table column
+        final int[] rowColorRGB = {198,233,255} ;//RGB ints for table row
+        final int[] rowSelectColorRGB = {105,186,255} ;//RGB ints for table row
+        int[] paddingLTRB = {10,10,10,10} ;//Padding Left, Top, Right, Bottom
+        String[] textViewHeader = {"OrderID","Site","Sitemanger","Total Price","Supplier"}; //Table Column Headers
+        String[][] tableData = {
+                {"B001","Mathara","Mr.Perera","$100","ABC"},
+                {"B002","Gampaha","Mr.Lalith","$100","ABC"},
+                {"B003","Colombo","Mr.Upul","$100","ABC"},
+                {"B004","Jaffna","Mr.Silva","$100","ABC"},
+                {"B005","Mathale","Mr.Kamal","$100","ABC"}
+        }; //Table Data
+
+        tableRowHeader = new TableRow(this);
+
+        for (int cnt = 0; cnt < textViewHeader.length; cnt++){
+            TextView header = new TextView(this);
+
+            header.setText(textViewHeader[cnt]);
+            header.setPadding(paddingLTRB[0], paddingLTRB[1], paddingLTRB[2], paddingLTRB[3]);
+            header.setWidth(textViewColumnWidth[cnt]);
+            header.setHeight(textViewRowHeight[0]);
+            header.setBackgroundColor(Color.rgb(headerColorRGB[0], headerColorRGB[1], headerColorRGB[2]));
+            header.setTextColor(Color.WHITE);
+
+            tableRowHeader.addView(header);
+        }
+        tableLayout.addView(tableRowHeader);
+
+        for (int row = 0; row < tableData.length; row++){
+
+            tableRowData = new TableRow(this);
+            tableRowData.setClickable(true);
+            tableRowData.setBackgroundColor(Color.rgb(rowColorRGB[0], rowColorRGB[1], rowColorRGB[2]));
+            final Boolean[] isRowSelect = {false};
+
+            tableRowData.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!isRowSelect[0]){
+                        v.setBackgroundColor(Color.rgb(rowSelectColorRGB[0], rowSelectColorRGB[1], rowSelectColorRGB[2]));
+
+                        TableRow selectedRow = (TableRow) v;
+                        TextView textView = (TextView) selectedRow.getChildAt(0);
+                        String result = textView.getText().toString();
+
+                        //Toast.makeText(Inquirylist.this, result, Toast.LENGTH_SHORT).show();
+                        Intent i1 = new Intent(Inquirylist.this,addEnquiry.class);
+                        i1.putExtra("orderID",result);
+                        startActivity(i1);
+
+
+                        isRowSelect[0] = true;
+
+                    }else {
+                        v.setBackgroundColor(Color.rgb(rowColorRGB[0], rowColorRGB[1], rowColorRGB[2]));
+                        isRowSelect[0] = false;
+                    }
+                }
+            });
+
+
+            for (int col = 0; col < tableData[0].length; col++){
+                TextView data = new TextView(this);
+
+                data.setText(tableData[row][col]);
+                data.setPadding(paddingLTRB[0], paddingLTRB[1], paddingLTRB[2], paddingLTRB[3]);
+                data.setWidth(textViewColumnWidth[col]);
+                data.setHeight(textViewRowHeight[1]);
+                data.setTextColor(Color.BLACK);
+
+                tableRowData.addView(data);
+
+            }
+
+            tableLayout.addView(tableRowData);
+        }
+
+    }
+}
